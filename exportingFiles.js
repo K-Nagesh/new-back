7a48f5995386631646013ab89db9f86dc9ff1465
unknown <knk.w3schools.com>
2022-10-17 23:13:51 -0700

    initial

---
+++ exportingFiles.js
@@ -0,0 +1,7 @@
+exports.add = async(a,b) =>{
+    console.log(a+b);
+};
+
+exports.sub = async(a,b) => {
+    console.log(a-b);
+};
---
+++ importingFiles.js
@@ -0,0 +1,4 @@
+const calculation = require('./exportingFiles')
+
+calculation.add(2,4)
+calculation.sub(4,2)
